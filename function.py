93cc2078b766fdf4862b0feb1f6c608249d3ba5a
kunpai <kunpai@ucdavis.edu>
2024-03-27 16:34:26 -0700

    Add test_function_2 to function.py

--- function.py
+++ function.py
@@ -4,3 +4,9 @@ def test_function():
     Docstring: This is a test function
     """
     print("This is a test function")
+
+def test_function_2():
+    """
+    Docstring: This is another test function
+    """
+    print("This is another test function")
