e896dd2222fce3f5dccd99f237cbed7c054e202d
kunpai <kunpai@ucdavis.edu>
2024-03-27 18:57:15 -0700

    Add test_function_3

--- function.py
+++ function.py
@@ -10,3 +10,7 @@ def test_function_2():
     Docstring: This is another test function
     """
     print("This is another test function")
+
+def test_function_3():
+    """ Docstring: This is a third test function """
+    print("This is a third test function")
